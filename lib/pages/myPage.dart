95747b57376522503604c1529fba2bf8c9852c25
gaox <gx_zone@126.com>
2020-04-17 01:19:40 +0800

    [dev] 定时器修改为 页面焦点时获取

--- lib/pages/myPage.dart
+++ lib/pages/myPage.dart
@@ -15,7 +15,6 @@ class MyPage extends StatefulWidget {
 Dio dio = new Dio();
 SomaTempVO _content = new SomaTempVO(0.0, 0.0);
 Timer timer;
-Function notice;
 
 class _MyPageState extends State<MyPage> {
   @override
@@ -50,44 +49,43 @@ class _MyPageState extends State<MyPage> {
 
   @override
   void initState() {
-    notice = () {
-      if (mounted) {
-        setState(() {});
-      }
-    };
-    _content.printPublic();
-    print("initStateBegin:${new DateTime.now()}");
-    timerTask();
-    print("initStateEnd:${new DateTime.now()}");
     super.initState();
+    //清除过时定时器
+    timer = null;
+    timerTask();
   }
-}
 
-void whenTaskCompelete() {
-  print("Task Complete");
-}
+  ///暂时离开页面
+  @override
+  void deactivate() {
+    super.deactivate();
+    //取消定时器方法
+    timer.cancel();
+  }
 
-void getRequest() async {
-  var response = await dio.get("http://iot.gaox.site/somaTemp/value/7");
-  ApiResponse api = ApiResponse.fromJson(response.data);
-  _content = SomaTempVO.fromJson(api.data);
-}
+  void whenTaskCompelete() {
+    print("Task Complete");
+  }
 
-void timerTask() {
-  new Future(() => null) //  异步任务的函数
-      .then((_) {
-    if (null == timer) {
-      timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
-        if (null != notice) {
-          notice();
-        }
-        getRequest();
-        // DateTime times = (DateTime.now()).toLocal();
-        // var soma = _content.updateTime;
-        // print("$times[===>$soma");
-        //取消定时器方法
-        // (timer as Timer).cancel();
-      });
-    }
-  }).whenComplete(() => whenTaskCompelete); //  当所有任务完成后的回调函数
+  void getRequest() async {
+    var response = await dio.get("http://iot.gaox.site/somaTemp/value/7");
+    ApiResponse api = ApiResponse.fromJson(response.data);
+
+    setState(() {
+      _content = SomaTempVO.fromJson(api.data);
+    });
+  }
+
+  void timerTask() {
+    new Future(() => null) //  异步任务的函数
+        .then((aa) {
+      if (null == timer) {
+        timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+          getRequest();
+        });
+      }
+    }).catchError((onError) {
+      print("catchError" + onError);
+    });
+  }
 }
